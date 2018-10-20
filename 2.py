f4bc9f39c46a98e6208e745c5122963e1d127e7f
edward-stan <dingyulong2000@outlook.com>
2018-10-20 21:33:02 +0800

    first commit

---
+++ 2.py
@@ -0,0 +1,2 @@
+name=input('请输入你的名字')
+print("what dosen't kill kill you makes you stronger")
---
+++ 5.py
@@ -0,0 +1,15 @@
+a=2.0
+print(id(a))
+print(a)
+a=2.0
+print(id(a))
+print(a)
+         
+####################################
+a=6
+print(id(a))
+print(a)
+b=a
+print(id(a))
+print(b)
+         
